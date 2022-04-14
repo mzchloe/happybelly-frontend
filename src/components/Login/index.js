ac7f575dad00a7513d12f9cad70d34fa147fd178
Jeanet Lin <linett@gmail.com>
2022-04-14 22:22:58 +0200

    login form basic

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,8 +2,13 @@ import styles from "./Login.module.css";
 
 export function Login() {
     return (
-        <form>
-
+        <form className={styles.login}>
+        <h2>Login</h2>
+             <input
+            id="email" placeholder="Your email"/>
+             <input
+            id="password" placeholder="Your password"/>
+            <button className="loginBtn">Login</button>
         </form>
     )
 }
