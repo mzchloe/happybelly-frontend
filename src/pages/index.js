c590fb32346f95e792dbfe677698f9de7c7da579
Jeanet Lin <linett@gmail.com>
2022-04-14 22:24:31 +0200

    export places and account

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { Home } from "./Home";
 export { LandingPage } from "./LandingPage";
 export { NotFound } from "./NotFound";
+export { Places } from "./Places";
+export { Account } from "./Account";
