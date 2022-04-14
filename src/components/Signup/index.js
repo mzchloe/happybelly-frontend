3886987fff50cccaf4d1f88b244e7bb9d2441957
Jeanet Lin <linett@gmail.com>
2022-04-14 22:24:14 +0200

    added form basic

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -2,8 +2,19 @@ import styles from "./Signup.module.css";
 
 export function Signup() {
     return (
-        <form>
-            
+        <form className={styles.signup}>
+        <h2>Create Account</h2>
+            <input
+            id="firstName" placeholder="Your first name"/>
+             <input
+            id="lastName" placeholder="Your last name"/>
+             <input
+            id="username" placeholder="Choose a username"/>
+             <input
+            id="email" placeholder="Your email"/>
+             <input
+            id="password" placeholder="Choose a strong password"/>
+            <button className="signupBtn">Let's go!</button>
         </form>
     )
 }
