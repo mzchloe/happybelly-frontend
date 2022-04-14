990bbce52ba9f4299d8c97deef083349b957412b
Jeanet Lin <linett@gmail.com>
2022-04-14 22:25:11 +0200

    placeholder content for landing page

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,9 +1,19 @@
 import styles from "./LandingPage.module.css";
+//import { Signup, Login } from "../../components"; 
+import { Link } from "react-router-dom";
 
 export function LandingPage() {
     return (
-        <div>
-            Landing Page
+        <div className={styles.landingPage}>
+          Welcome to Happybelly
+        <p> 
+        <button className={styles.landingPageBtn}>
+        <Link to="/login">Signup</Link>
+        </button>
+        <button className={styles.landingPageBtn}>
+        <Link to="/login">Login</Link></button>
+        </p>
+       
         </div>
     )
 }
