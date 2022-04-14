d727bd90e96c9780c4e9131ccee1cda40d83f0a2
Jeanet Lin <linett@gmail.com>
2022-04-14 22:22:19 +0200

    imported footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { Navbar } from "./Navbar";
+export { Footer } from "./Footer";
 export { Signup } from "./Signup";
 export { Login } from "./Login";
 export { PrivateRoute } from "./PrivateRoute";
