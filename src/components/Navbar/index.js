01dd4452ad2d037baac1604266eca648444831e8
Jeanet Lin <linett@gmail.com>
2022-04-16 09:48:37 +0200

    added icons placeholders

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
+import { MdAccountCircle, MdOutlineLogout, MdFavorite, MdPinDrop, MdHome } from "react-icons/md";
 
 export function Navbar() {
     return (
@@ -9,10 +10,11 @@ export function Navbar() {
         <Link className={styles.link} to="/signup">Signup</Link> 
         <Link className={styles.link} to="/login">Login</Link> 
         {/* display when user is logged in */}
-            <Link className={styles.link} to="/home">Home</Link> 
-            <Link className={styles.link} to="/places">Places</Link> 
-            <Link className={styles.link} to="/account">My Account</Link> 
-            <Link className={styles.link} to="/">Logout</Link> 
+            <Link className={styles.link} to="/home"><MdHome /></Link> 
+            <Link className={styles.link} to="/places"><MdPinDrop /></Link> 
+            <Link className={styles.link} to="/favorites"><MdFavorite /></Link> 
+            <Link className={styles.link} to="/account"> <MdAccountCircle /></Link> 
+            <Link className={styles.link} to="/"><MdOutlineLogout /></Link> 
         </nav>
     )
 }
