21118c2fd7dc957cc0a56b36e3a33804f1d94d37
Jeanet Lin <linett@gmail.com>
2022-04-16 22:08:18 +0200

    adding user context

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,14 +1,20 @@
 import styles from "./LandingPage.module.css";
 //import { Signup, Login } from "../../components"; 
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context";
 
 export function LandingPage() {
+  //get the user 
+  const {user} = useContext(AuthContext);
     return (
-        <div className={styles.landingPage}>
-          Welcome to Happybelly
+        <div className={styles.landingPageContainer}>
+         <code>{JSON.stringify(user)}</code>
+       {/*   <Login />
+         <Signup /> */}
         <p> 
         <button className={styles.landingPageBtn}>
-        <Link to="/login">Signup</Link>
+        <Link to="/signup">Signup</Link>
         </button>
         <button className={styles.landingPageBtn}>
         <Link to="/login">Login</Link></button>
