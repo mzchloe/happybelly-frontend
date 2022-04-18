1a450106a11d40c2b46658eb370f0e0a0e90eae0
Jeanet Lin <linett@gmail.com>
2022-04-19 01:06:56 +0200

    added logout

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,45 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
-import { MdAccountCircle, MdOutlineLogout, MdFavorite, MdPinDrop, MdHome } from "react-icons/md";
+import { AuthContext } from "../../context";
+import { useContext } from "react";
+import {
+  MdAccountCircle,
+  MdOutlineLogout,
+  MdFavorite,
+  MdPinDrop,
+  MdHome,
+} from "react-icons/md";
 
 export function Navbar() {
-    return (
-        <nav>
-        {/* Show when visitor comes to page */}
-        <Link className={styles.link} to="/signup">Signup</Link> 
-        <Link className={styles.link} to="/login">Login</Link> 
-        {/* display when user is logged in */}
-            <Link className={styles.link} to="/home"><MdHome /></Link> 
-            <Link className={styles.link} to="/places"><MdPinDrop /></Link> 
-            <Link className={styles.link} to="/favorites"><MdFavorite /></Link> 
-            <Link className={styles.link} to="/account"> <MdAccountCircle /></Link> 
-            <Link className={styles.link} to="/"><MdOutlineLogout /></Link> 
-        </nav>
-    )
+    const {logout} = useContext(AuthContext);
+  return (
+    <nav>
+      {/* Show when visitor comes to page */}
+      <Link className={styles.link} to="/signup">
+        Signup
+      </Link>
+      <Link className={styles.link} to="/login">
+        Login
+      </Link>
+      {/* display when user is logged in */}
+      <Link className={styles.link} to="/home">
+        <MdHome />
+      </Link>
+      <Link className={styles.link} to="/places">
+        <MdPinDrop />
+      </Link>
+      <Link className={styles.link} to="/favorites">
+        <MdFavorite />
+      </Link>
+      <Link className={styles.link} to="/account">
+        <MdAccountCircle />
+      </Link>
+      <Link className={styles.link} to="/">
+        <button onClick={logout}>
+          <MdOutlineLogout />
+        </button>
+      </Link>
+    </nav>
+  );
 }
