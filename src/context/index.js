1dd4f071c5716d91cbc08837994080dbe55ce560
Jeanet Lin <linett@gmail.com>
2022-04-18 13:03:28 +0200

    added the rerouting after login and signup

--- src/context/index.js
+++ src/context/index.js
@@ -1,9 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { client } from "../client";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export function AuthContextProvider({ children }) {
+  //navigating programatically
+  const navigate = useNavigate();
+  //setting the user
   const [user, setUser] = useState(null);
 
   //saving the token
@@ -13,14 +17,19 @@ export function AuthContextProvider({ children }) {
 
   //signup function 
   const signup = async (firstName, lastName, email, username, password) => {
-    // console.log(firstName, lastName, email, username, password);
-    const response = await client.post("/auth/signup", {
-      firstName,
-      lastName,
-      email,
-      username,
-      password,
-    });
+   try {
+// console.log(firstName, lastName, email, username, password);
+const response = await client.post("/auth/signup", {
+  firstName,
+  lastName,
+  email,
+  username,
+  password,
+});
+navigate('/login')
+   } catch (error) {
+     console.log(error)
+   }
   };
 
   //login function 
@@ -33,10 +42,29 @@ export function AuthContextProvider({ children }) {
       saveToken(response.data.token);
       //set the user
       setUser(response.data.user);
+      //once user is logged in, we redirect the user
+      navigate('/home')
     } catch (error) {
       console.log(error);
     }
   };
+
+  //verify function 
+  const verify = async () => {
+    try { 
+      const response = await client.get('auth/verify')
+      setUser (response.data.user)
+      //if user is correct, we redirect the user to
+      navigate('/home')
+    } catch (error) {
+      navigate('/')
+    }
+  }
+
+  //to execute our verify function just once when the app starts
+  useEffect(() => {
+    verify();
+  }, []);
 
   //values accessible everywhere in the app
   const value = {
@@ -44,6 +72,7 @@ export function AuthContextProvider({ children }) {
     signup,
     login,
   };
+
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
