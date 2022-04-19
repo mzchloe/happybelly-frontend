99343967df17384456d76e885b8afb385b5001f5
Jeanet Lin <linett@gmail.com>
2022-04-19 21:54:26 +0200

    Retrieving list of places

--- src/pages/Places/index.js
+++ src/pages/Places/index.js
@@ -1,8 +1,25 @@
 import styles from "./Places.module.css";
+import { useEffect, useState } from "react";
+import { client } from "../../client";
+import { ListOfPlaces } from "../../components";
 
 export function Places() {
+    const [places, setPlaces] = useState([])
+
+    const getPlaces = async () => {
+        const result = await client.get('/place');
+
+        setPlaces(result.data);
+    };
+//console.log(places)
+    useEffect(() => {
+        getPlaces();
+      }, []);
+
     return (
-        <div>List of all places</div>
+        <div>
+            <ListOfPlaces places={places}/>
+        </div>
         
     )
 }
