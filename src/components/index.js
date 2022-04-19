418fc6513162db226976c84f8a146a5e199c7eaf
Jeanet Lin <linett@gmail.com>
2022-04-19 21:56:26 +0200

    adding two new components ListOfPlaces and Place

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { Footer } from "./Footer";
 export { Signup } from "./Signup";
 export { Login } from "./Login";
 export { PrivateRoute } from "./PrivateRoute";
+export { ListOfPlaces } from "./ListOfPlaces";
+export { Place } from "./Place";
