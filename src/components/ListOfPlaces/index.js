6040cf2497453a76fea8518fbc5823e469d21b24
Jeanet Lin <linett@gmail.com>
2022-04-19 21:56:10 +0200

    Creating a Place component for List of Places

--- src/components/ListOfPlaces/index.js
+++ src/components/ListOfPlaces/index.js
@@ -1,9 +1,19 @@
 import styles from "./ListOfPlaces.module.css";
+import { Place } from "../Place";
+import { client } from "../../client";
 
-export function ListOfPlaces() {
-    return (
-       <ul>
-       List of places
-       </ul>
-    )
+export function ListOfPlaces({ places }) {
+  return (
+    <ul className={styles.listContainer}>
+      List of places
+      {places.map((place) => {
+        return (
+          <div key={place._id}>
+            <Place place={place}
+            />
+          </div>
+        );
+      })}
+    </ul>
+  );
 }
