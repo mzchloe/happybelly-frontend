fee7678e86012940590e35095a52e0046e1bb14e
Jeanet Lin <linett@gmail.com>
2022-04-21 22:20:56 +0200

    display My Places

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,8 +1,14 @@
 import styles from "./Account.module.css";
+import { MyPlaces } from "../MyPlaces";
 
 export function Account() {
-    return (
-        <div>Your Account Page</div>
-        
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.discover}>Discover Places</div>
+      <div className={styles.favorites}>My Favorites</div>
+      <div className={styles.myPlaces}>
+        <MyPlaces />
+      </div>
+    </div>
+  );
 }
