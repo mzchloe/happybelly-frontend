0a53b850b5ee5bb093ad7e545cf0bcb4a713cf1e
Jeanet Lin <linett@gmail.com>
2022-04-21 22:21:35 +0200

    MyPlaces function

--- src/pages/MyPlaces/index.js
+++ src/pages/MyPlaces/index.js
@@ -1,8 +1,40 @@
 import styles from "./MyPlaces.module.css";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../context";
+import { client } from "../../client";
+import { Place } from "../../components/Place";
 
 export function MyPlaces() {
+    const [places, setPlaces] = useState([])
+   // const { user } = useContext(AuthContext)
+
+    const getPlaces = async () => {
+        const result = await client.get('/place/myplaces');
+        //the data
+        setPlaces(result.data);
+    };
+
+//making request backend to run only once 
+    useEffect(() => {
+        getPlaces();
+      }, []);
+
+    //get the logged in user (get the user from useContext?)
+    //get all the places id by this user (findById)
+   
+
     return (
-        <div>My Restaurants</div>
         
+        <ul className={styles.listContainer}>
+      My Places
+      {places.map((place) => {
+        return (
+          <div key={place._id}>
+          <Place place={place}/>
+          </div>
+        );
+      })}
+    </ul>
+         //display them 
     )
 }
