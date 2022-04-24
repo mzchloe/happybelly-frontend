4ae1466fefccf08d64f83453e62d41240355f2de
Jeanet Lin <linett@gmail.com>
2022-04-24 22:32:58 +0200

    code edit

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -2,7 +2,7 @@ import styles from "./Favorites.module.css";
 
 export function Favorites() {
     return (
-        <div>My Favorite Places</div>
+        <div className={styles.container}>My Favorite Places</div>
         
     )
 }
