b12b37f00ff0f071c357b09267268bcf9c60f55b
Jeanet Lin <linett@gmail.com>
2022-04-24 22:26:27 +0200

    export compnents

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,10 @@ export { Login } from "./Login";
 export { PrivateRoute } from "./PrivateRoute";
 export { ListOfPlaces } from "./ListOfPlaces";
 export { Place } from "./Place";
+export { PlaceForm } from "./PlaceForm";
+export { EditPlace } from "./EditPlace";
+export {AddComment} from "./AddComment";
+export {Comment} from "./Comment";
+export { Copyright } from "./Copyright";
+
+
