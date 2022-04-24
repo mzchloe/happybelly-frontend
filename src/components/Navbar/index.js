c9862ea33ba9dea24147177218404f781bdd2e9f
Jeanet Lin <linett@gmail.com>
2022-04-24 22:29:50 +0200

    changed navbar to just logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,47 +1,28 @@
 import React from "react";
 import styles from "./Navbar.module.css";
-import { Link } from "react-router-dom";
-import { AuthContext } from "../../context";
-import { useContext } from "react";
-import {
-  MdAccountCircle,
-  MdOutlineLogout,
-  MdFavorite,
-  MdPinDrop,
-  MdHome,
-} from "react-icons/md";
+import logo from "../../happybelly.svg";
 
 export function Navbar() {
-    const { user } = useContext(AuthContext)
-    const {logout} = useContext(AuthContext);
+   
   return (
-    <nav>
-      {/* Show when visitor comes to page */}
-      <Link className={styles.link} to="/signup">
+    <div className={styles.nav}>
+     
+   {/*   <Link className={styles.link} to="/signup">
         Signup
       </Link>
       <Link className={styles.link} to="/login">
         Login
-      </Link>
-      {/* display when user is logged in */}
-      
-      <Link className={styles.link} to="/home">
-        <MdHome />
-      </Link>
-      <Link className={styles.link} to="/places">
-        <MdPinDrop />
-      </Link>
-      <Link className={styles.link} to="/favorites">
-        <MdFavorite />
-      </Link>
-      <Link className={styles.link} to="/account">
-        <MdAccountCircle />
-      </Link>
-      <Link className={styles.link} to="/">
-        <button onClick={logout}>
-          <MdOutlineLogout />
-        </button>
-      </Link>
-    </nav>
+      </Link> */}
+      <div className={styles.image}>
+      <img
+          width={150}
+          height={90}
+          className="logo__img"
+          src={logo}
+          alt="logo_image"
+        />
+     </div>
+     
+    </div>
   );
 }
