73f1ba4528ae0f9a0c1eb27d952fc4e83b1152ea
Jeanet Lin <linett@gmail.com>
2022-04-24 22:33:32 +0200

    WIP landing page content and css

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,19 +1,30 @@
 import styles from "./LandingPage.module.css";
 //import { Signup, Login } from "../../components";
 import { Link } from "react-router-dom";
+import photo from "../../img/hero.jpg";
 
 
 export function LandingPage() {
 
   return (
     <div className={styles.landingPageContainer}>
+    <div className={styles.hero}>
+    <div className={styles.heroText}>  Discover the places that will make sure you 
+can  eat with a 
+happy belly</div>
+<div className={styles.signupBox}>
+        <Link to="/signup">
+        <button className={styles.joinBtn}>JOIN FOR FREE</button></Link>
+
+      </div>
+
+</div>
       {/*   <Login />
          <Signup /> */}
-      <div className={styles.signupBox}>
-        <Link to="/signup">JOIN FOR FREE</Link>
-      </div>
+      
       <div className={styles.loginBox}>
-        <Link to="/login">Already Member?<p>Login Here</p></Link>
+        <Link to="/login" className={styles.text}><p>Already member?
+        <button className={styles.loginBtn}>Log In</button></p></Link>
       </div>
     </div>
   );
