c34cadeb0f393a42d3981141e79e106f585feeca
Jeanet Lin <linett@gmail.com>
2022-04-24 22:34:11 +0200

    created delete function

--- src/pages/Places/index.js
+++ src/pages/Places/index.js
@@ -1,25 +1,48 @@
 import styles from "./Places.module.css";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { client } from "../../client";
 import { ListOfPlaces } from "../../components";
+import { AuthContext } from "../../context";
+import searchIcon from "../../img/magnifier.png";
 
 export function Places() {
-    const [places, setPlaces] = useState([])
+    const { places, getPlaces } = useContext(AuthContext);
+    
+    
+    //const [places, setPlaces] = useState([])
 
-    const getPlaces = async () => {
+   /*  const getPlaces = async () => {
         const result = await client.get('/place');
 
         setPlaces(result.data);
-    };
+    }; replaced by useContext above */
+
+//this function is moved from Place component
+    const handleDelete = async (id) => {
+      /*   const filterPlaces = places.filter((place) => {
+            return place._id !== id
+        })  */
+      
+        await client.delete(`/place/${id}`);
+        getPlaces()
+      };
+    
 //console.log(places)
     useEffect(() => {
         getPlaces();
       }, []);
 
     return (
-        <div>
-            <ListOfPlaces places={places}/>
+        <div className={styles.container}>
+        <div className={styles.search}>
+            <input className={styles.input} type="search" placeholder="Search by restaurant name or city">
+          
+            </input>
         </div>
+        {/* <div className={styles.places}> */}
+            <ListOfPlaces places={places} handleDelete={handleDelete}/>
+        </div>
+        /* </div> */
         
     )
 }
