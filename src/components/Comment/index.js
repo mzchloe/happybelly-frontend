622e8f6346b669fde11b1816ad09ca691f1305eb
Jeanet Lin <linett@gmail.com>
2022-04-26 23:18:36 +0200

    adding delete function

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,9 +1,30 @@
 import styles from "./Comment.module.css";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context";
+import { client } from "../../client";
 
-export function Comment({comment}) {
-    return (
-        <div className={styles.commentCard}>
-            <p>{comment.comment}</p>
-        </div>
-    )
+export function Comment({ comment }) {
+  const { user, getPlaces } = useContext(AuthContext);
+
+  const handleDelete = async (id) => {
+  
+      await client.delete(`/comment/${id}`);
+      getPlaces();
+    };
+
+  return (
+      
+    <div className={styles.commentCard}>
+      <div className={styles.commentBody}>
+        <span className={styles.postedBy}>Commented on {comment.createdAt.split("T")[0]} by {comment.author.firstName} {comment.author.lastName}</span>
+        <p className={styles.comment}>{comment.comment}</p>
+      </div>
+      <div className={styles.btns}>
+        {user._id === comment.author._id &&  
+        <button onClick={() => handleDelete(comment._id)}>Delete</button>}
+
+      </div>
+    </div>
+    
+  );
 }
