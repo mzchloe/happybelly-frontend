f9cc3ec0ff12a1aab6a7c66d651af741d44dff9e
Jeanet Lin <linett@gmail.com>
2022-04-26 23:19:00 +0200

    show/hide comments and comment box

--- src/components/Place/index.js
+++ src/components/Place/index.js
@@ -5,7 +5,7 @@ import {
   MdOutlineAddComment,
   MdOutlineModeComment,
 } from "react-icons/md";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context";
 import { client } from "../../client";
 import { useNavigate } from "react-router-dom";
@@ -17,43 +17,74 @@ export function Place({ place, handleDelete }) {
   const { user } = useContext(AuthContext);
   //const [place, setPlace] = useState("");
 
-  
+  const [addComment, setAddComment] = useState(false);
+
+  const [showComment, setShowComment] = useState(false);
+
   //console.log(user._id, place.author._id, place._id)
-let navigate = useNavigate()
+  let navigate = useNavigate();
 
   const handleEdit = () => {
-    navigate(`/editPlace/${place._id}`)
-   // client.put(`/place/${place._id}`);
+    navigate(`/editPlace/${place._id}`);
+    // client.put(`/place/${place._id}`);
+  };
 
+  const toggleComment = () => {
+    setAddComment((previousValue) => {
+      return !previousValue; //return true
+    });
+  };
+
+  const toggleShowComment = () => {
+    setShowComment((previousValue) => {
+      return !previousValue;
+    });
   };
 
   return (
     <div className={styles.placeCard}>
-        {user._id === place.author._id && (
-        <button onClick={() => {handleDelete(place._id)}}>Delete</button>
+ <div className={styles.privateBtns}> 
+      {user._id === place.author._id && (
+        <button
+          className={styles.delete}
+          onClick={() => {
+            handleDelete(place._id);
+          }}>
+          <MdDelete />
+        </button>
       )}
       {user._id === place.author._id && (
-        <button onClick={handleEdit}>Edit</button>
-      )}
+        <button className={styles.edit} onClick={handleEdit}>
+          <MdModeEdit />
+        </button>
+        )}
+        </div>
+      
       <h3 className={styles.name}>{place.name}</h3>
       <span className={styles.city}>{place.city}</span>
       <span className={styles.author}>
         Author: {place.author.firstName} {place.author.lastName}
       </span>
-      <span className={styles.published}>Published: {place.createdAt}</span>
+      <span className={styles.published}>
+        Published: {place.createdAt.split("T")[0]}
+      </span>
+      {/* <span className={styles.published}>Published: {place.createdAt.split(".")[0].split("T").join(" at ")} </span> */}
       <span className={styles.diet}>Dietary Type:{place.dietaryType}</span>
       <p className={styles.description}>{place.description}</p>
-      
-      {user._id && <button>Comment</button>}
-      <button>Show Comments</button>
+
+      {user._id && <button onClick={toggleComment}> Comment</button>}
+      <button onClick={toggleShowComment}>Show Comments</button>
       {/* <div className={styles.comments}> */}
       {/* <MdOutlineAddComment />
           <MdOutlineModeComment /> */}
-          <AddComment place={place} />
-          <ul>
-            {place.comments.map((comment) => <Comment key={comment._id} comment={comment}/>)}
-          </ul>
-          
+      {addComment && <AddComment place={place} />}
+      {showComment && (
+        <ul>
+          {place.comments.map((comment) => (
+            <Comment key={comment._id} comment={comment} />
+          ))}
+        </ul>
+      )}
     </div>
     /* </div> */
   );
