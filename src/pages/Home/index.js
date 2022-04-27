78c948e6f995c9c00b39e9368e30981d8ae15b7b
Jeanet Lin <linett@gmail.com>
2022-04-27 22:12:54 +0200

    added link to pages

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import styles from "./Home.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context";
 import { PlaceForm } from "../../components/PlaceForm";
 import learn from "../../img/learn.png";
@@ -7,24 +7,34 @@ import restaurant from "../../img/restaurant.png";
 import tips from "../../img/tips.png";
 import city from "../../img/city.png";
 import community from "../../img/community.png";
+import { Link } from "react-router-dom";
 
 
 export function Home() {
   //get the user
   const { user } = useContext(AuthContext);
 
+/* Attempt for displaying "Coming Soon" when clicked on 
+  const [comingSoon, setComingSoon] = useState(true);
+
+  const toggleCategory = () => {
+    setComingSoon((previousValue) => {
+      return <span> coming soon</span>; //return true
+    });
+  };
+ */
   return (
     <div className={styles.container}>
       {user ? <h2>Welcome {user.firstName}</h2> : null}
       <div className={styles.categoryDiscoverCard}>
       <div className={styles.image}>
-        <img
+      <Link className={styles.link} to="/places"><img
           width={60}
           height={60}
           className={styles.restaurant__img}
           src={restaurant}
           alt="restaurant_image"
-        />
+        /></Link>
         </div>
         <div className={styles.category}>
           <h3>DISCOVER</h3>
@@ -79,7 +89,7 @@ export function Home() {
         </div>
       </div>
       <div className={styles.categoryCommunityCard}>
-      <div className={styles.image}>
+     <div className={styles.image}>
         <img
           width={60}
           height={60}
