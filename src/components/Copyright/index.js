17e2afa961f0b24370fb4e0d00b5d7b239da00cd
Jeanet Lin <linett@gmail.com>
2022-04-27 22:07:15 +0200

    added link in copyright

--- src/components/Copyright/index.js
+++ src/components/Copyright/index.js
@@ -1,7 +1,13 @@
 import styles from "./Copyright.module.css";
+import { Link } from "react-router-dom";
 
 export function Copyright() {
-    return (
-        <div className={styles.copyright}> ABOUT © 2022 JEANET LIN</div>
-    )
+  return (
+    <div className={styles.copyright}>
+      <Link className={styles.link} to="/about">
+        ABOUT
+      </Link>
+      © 2022 JEANET LIN
+    </div>
+  );
 }
