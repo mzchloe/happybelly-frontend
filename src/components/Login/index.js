40dc4414df906d1b5cac6b2e1a32aad9f9773993
Jeanet Lin <linett@gmail.com>
2022-04-27 16:46:10 +0200

    edit login layout and css

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,29 +1,52 @@
 import styles from "./Login.module.css";
 import { AuthContext } from "../../context";
 import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../img/happybelly.svg";
 
 export function Login() {
-    const {login} = useContext(AuthContext);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const { login } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        login(email, password);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
 
-    return (
-        <form onSubmit={handleSubmit} className={styles.login}>
-        <h2>Login</h2>
-             <input
-            id="email" type="email" value={email} onChange={(e) => {
-                setEmail(e.target.value);
-            }} placeholder="Your email"/>
-             <input
-            id="password" type="password" value={password} onChange={(e) => {
-                setPassword(e.target.value);
-            }} placeholder="Your password"/>
-            <button className="loginBtn">Login</button>
-        </form>
-    )
+  return (
+    <div className={styles.formContainer}>
+      <form onSubmit={handleSubmit} className={styles.login}>
+        {/* <h2>Login</h2> */}
+        <Link to="/">
+          <img
+            width={250}
+            height={150}
+            className={styles.logo__img}
+            src={logo}
+            alt="logo_image"
+          />
+        </Link>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="Your email"
+        />
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="Your password"
+        />
+        <button className={styles.loginBtn}>Login</button>
+      </form>
+    </div>
+  );
 }
