90d8d4e6ddfe399e60825bbafde09725a2399650
Jeanet Lin <linett@gmail.com>
2022-04-27 22:06:14 +0200

    added about page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home, Places, Account, LandingPage, Favorites, NotFound } from "./pages";
+import { Home, About, Places, Account, LandingPage, Favorites, NotFound } from "./pages";
 import { Signup, Login, EditPlace, PlaceForm} from "./components";
 import { AuthContextProvider } from "./context";
 
@@ -20,7 +20,7 @@ root.render(
             <Route path="signup" element={<Signup />} />
             <Route path="login" element={<Login />} />
             <Route path="places" element={<Places />} />
-          
+            <Route path="about" element={<About />} />
             <Route path="favorites" element={<Favorites />} />
             <Route path="add" element={<PlaceForm />} />
             <Route path="editPlace/:id" element={<EditPlace />} />
