b144bce86e9bb465aba06aea045962528632cd26
Jeanet Lin <linett@gmail.com>
2022-04-28 22:56:57 +0200

    added profile and edited structure

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -4,6 +4,7 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../../context";
 import { Link } from "react-router-dom";
 import { MdOutlineLogout } from "react-icons/md";
+import profile from "../../img/woman.png";
 
 export function Account() {
   const { user } = useContext(AuthContext);
@@ -18,20 +19,41 @@ export function Account() {
   };
 
   return (
-
     <div className={styles.container}>
-    <Link className={styles.link} onClick={logout} to="/" >
-          <MdOutlineLogout />
-      </Link>
-    
-{/*       <div className={styles.discover}>
-      <h3>Discover Places</h3></div> */}
       <div className={styles.favorites}>
-      <h3>My Favorites</h3></div>
-      <div className={styles.myPlaces}>
-      <button className={styles.reviews} onClick={toggleReviews}><h3>My Reviews</h3></button>
-       {myReviews && <MyPlaces />}
+        <h3>My Profile</h3>
+        <div className={styles.profile}>
+          <img
+            width={60}
+            height={60}
+            className={styles.profile__img}
+            src={profile}
+            alt="profile_image"
+          />
+          <span>
+            <b>
+              {user.firstName} {user.lastName}
+            </b>
+          </span>
+          <span>Username {user.username}</span>
+          <span>Email: {user.email}</span>
+          <span>Change Password </span>
+        </div>
+        <h3>My Favorites</h3>
       </div>
+
+      {/* My Reviews  */}
+      <button className={styles.reviews} onClick={toggleReviews}>
+        <h3>My Reviews</h3>
+      </button>
+      {myReviews && <MyPlaces />}
+
+      {/* Logout  */}
+      <button className={styles.logout}>
+        <Link className={styles.link} onClick={logout} to="/">
+          <MdOutlineLogout /> Logout
+        </Link>
+      </button>
     </div>
   );
 }
