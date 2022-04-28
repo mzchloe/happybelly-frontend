9d04b415d82d3034e3b58946f4de04f3d08c618c
Jeanet Lin <linett@gmail.com>
2022-04-28 22:54:54 +0200

    added logo and link to /home

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,28 +1,28 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import logo from "../../happybelly.svg";
+import { Link } from "react-router-dom";
 
 export function Navbar() {
-   
   return (
     <div className={styles.nav}>
-     
-   {/*   <Link className={styles.link} to="/signup">
+      {/*   <Link className={styles.link} to="/signup">
         Signup
       </Link>
       <Link className={styles.link} to="/login">
         Login
       </Link> */}
-      <div className={styles.image}>
-      <img
-          width={150}
-          height={90}
-          className="logo__img"
-          src={logo}
-          alt="logo_image"
-        />
-     </div>
-     
+      <Link className={styles.link} to="/home">
+        <div className={styles.image}>
+          <img
+            width={150}
+            height={90}
+            className="logo__img"
+            src={logo}
+            alt="logo_image"
+          />
+        </div>
+      </Link>
     </div>
   );
 }
