0cda4e0dc917e8c74ba5eaa4e0a1b2cf1fd4d37c
Jeanet Lin <linett@gmail.com>
2022-04-28 22:56:35 +0200

    added content

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,10 +1,37 @@
 import styles from "./About.module.css";
 import logo from "../../img/happybelly.svg";
+import { useContext } from "react";
+import { AuthContext } from "../../context";
+import { Link } from "react-router-dom";
 
 export function About() {
+  const { user } = useContext(AuthContext);
   return (
     <div className={styles.about}>
-    happy belly is made for people living with Irritable Bowel Syndrome 
+      {/* Welcome to happybelly! */}
+      {!user && (
+        <div className={styles.image}>
+          <Link className={styles.link} to="/">
+            {" "}
+            <img
+              width={150}
+              height={90}
+              className="logo__img"
+              src={logo}
+              alt="logo_image"
+            />{" "}
+          </Link>
+        </div>
+      )}
+      <p>Hi there! I'm Jeanet and this is my first React App with a mobile-first design
+      that I have developed as a final project for Ironhack's full-stack web development bootcamp.</p>
+      <p>
+        <i>happybelly</i> is designed for foodies, like me, living with
+        Irritable Bowel Syndrome. This is the first and only community for sharing and 
+        discovering restaurants around the world that are IBS-friendly.
+      </p>
+      <p></p>
+    
     </div>
   );
 }
