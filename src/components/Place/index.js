47f53014187b50d915e635c4382cfc01293c3724
Jeanet Lin <linett@gmail.com>
2022-04-29 22:29:08 +0200

    added img to card

--- src/components/Place/index.js
+++ src/components/Place/index.js
@@ -2,7 +2,6 @@ import styles from "./Places.module.css";
 import {
   MdDelete,
   MdModeEdit,
-  MdOutlineAddComment,
   MdOutlineModeComment,
   MdOutlineFavorite,
   MdOutlineFavoriteBorder,
@@ -13,6 +12,7 @@ import { client } from "../../client";
 import { useNavigate } from "react-router-dom";
 import { AddComment } from "../AddComment";
 import { Comment } from "../Comment";
+import restaurant from "../../img/restaurant.png";
 
 export function Place({ place, handleDelete }) {
   //getting the user
@@ -20,11 +20,9 @@ export function Place({ place, handleDelete }) {
   //const [place, setPlace] = useState("");
 
   const [addComment, setAddComment] = useState(false);
-
   const [showComment, setShowComment] = useState(false);
-
   const [isFavorite, setIsFavorite] = useState(false);
-
+  const [showAll, setShowAll] = useState(false);
 
   //console.log(user._id, place.author._id, place._id)
   let navigate = useNavigate();
@@ -40,12 +38,12 @@ export function Place({ place, handleDelete }) {
       userId: user._id,
       placeId: place._id,
     });
-    console.log(newUser)
+    console.log(newUser);
     setUser(newUser.data);
     //navigate('/favorites');
   };
 
-/*   const handleUnSave = () => {
+  /*   const handleUnSave = () => {
     //request to backend by sending the userId and placeId from frontend
     const removePlace = client.put("/user/favorites", {
       userId: user._id,
@@ -55,11 +53,11 @@ export function Place({ place, handleDelete }) {
     //navigate('/favorites');
   }; */
 
-  const toggleComment = () => {
+  /*  const toggleComment = () => {
     setAddComment((previousValue) => {
       return !previousValue; //return true
     });
-  };
+  }; */
 
   const toggleShowComment = () => {
     setShowComment((previousValue) => {
@@ -69,70 +67,83 @@ export function Place({ place, handleDelete }) {
 
   return (
     <div className={styles.placeCard}>
-    {console.log(user, 'this is our user')}
-      <div className={styles.privateBtns}>
-        {user._id === place.author._id && (
-          <button
-            className={styles.delete}
-            onClick={() => {
-              handleDelete(place._id);
-            }}
-          >
-            <MdDelete />
-          </button>
-        )}
-        {user._id === place.author._id && (
-          <button className={styles.edit} onClick={handleEdit}>
-            <MdModeEdit />
-          </button>
-        )}
-        {/* {user._id && console.log(place._id, user.favorite.forEach(savedPlace))} */}
-        {user._id && user.favorite.forEach((savedPlace) => {
-          console.log(savedPlace, place._id)
-          if (savedPlace === place._id) {
-            if (isFavorite === false ) {
-              setIsFavorite(true);
-            } 
-           
-          }
-        })}
-        
-        {user._id && 
-        isFavorite ? 
-          <button
-                className={styles.save}
-              >
-                <MdOutlineFavorite />
-              </button>
-            :
-              <button
-                className={styles.save}
-                onClick={() => {
-                  handleSave();
-                }}
-              >
-                <MdOutlineFavoriteBorder />
-              </button>  
-        }
+      {/* {console.log(user, 'this is our user')} */}
+      <div className={styles.header}>
+        <div className={styles.image}>
+          <img
+            width={60}
+            height={60}
+            className={styles.restaurant__img}
+            src={restaurant}
+            alt="restaurant_image"
+          />
+        </div>
+        <div className={styles.place}>
+          <h3>{place.name}</h3>
+          <span>{place.city}</span>
+        </div>
+        <div className={styles.privateBtns}>
+          {user._id === place.author._id && (
+            <button
+              className={styles.delete}
+              onClick={() => {
+                handleDelete(place._id);
+              }}
+            >
+              <MdDelete />
+            </button>
+          )}
+          {user._id === place.author._id && (
+            <button className={styles.edit} onClick={handleEdit}>
+              <MdModeEdit />
+            </button>
+          )}
+          {/* {user._id && console.log(place._id, user.favorite.forEach(savedPlace))} */}
+        </div>
       </div>
 
-      <h3 className={styles.name}>{place.name}</h3>
-      <span className={styles.city}>{place.city}</span>
-      <span className={styles.author}>
-        Added by: {place.author.firstName} {place.author.lastName}
-      </span>
-      <span className={styles.published}>
-        Created on: {place.createdAt.split("T")[0]}
-      </span>
       {/* <span className={styles.published}>Published: {place.createdAt.split(".")[0].split("T").join(" at ")} </span> */}
-      <span className={styles.diet}>Special diet: {place.dietaryType}</span>
-      <p className={styles.description}>{place.description}</p>
+      <span className={styles.diet}>{place.dietaryType}</span>
+      <div className={styles.details}>
+        {place.author.firstName} {place.author.lastName} on{" "}
+        {place.createdAt.split("T")[0]}
+      </div>
+      <div className={styles.description}>
+        <p>{place.description}</p>
+      </div>
 
-     {/*  {user._id && <button onClick={toggleComment}> Add Comment</button>} */}
-      <button onClick={toggleShowComment}> Comments</button>
-      {/* <div className={styles.comments}> */}
-      {/* <MdOutlineAddComment />
-          <MdOutlineModeComment /> */}
+      <div className={styles.bottom}>
+        <div className={styles.favoriteBtn}>
+          {user._id &&
+            user.favorite.forEach((savedPlace) => {
+              if (savedPlace === place._id) {
+                if (isFavorite === false) {
+                  setIsFavorite(true);
+                }
+              }
+            })}
+
+          {user._id && isFavorite ? (
+            <button className={styles.save}>
+              <MdOutlineFavorite /> Added to Favourite
+            </button>
+          ) : (
+            <button
+              className={styles.save}
+              onClick={() => {
+                handleSave();
+              }}
+            >
+              <MdOutlineFavoriteBorder /> Add to Favourite
+            </button>
+          )}
+        </div>
+        <div className={styles.comment} onClick={toggleShowComment}>
+          <MdOutlineModeComment />
+          <button className={styles.commentBtn}>Comments</button>
+        </div>
+      </div>
+
       {showComment && <AddComment place={place} />}
       {showComment && (
         <ul>
@@ -142,6 +153,5 @@ export function Place({ place, handleDelete }) {
         </ul>
       )}
     </div>
-    /* </div> */
   );
 }
