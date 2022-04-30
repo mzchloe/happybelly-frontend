8ffde6f9623b1d7a25640f9c2f385047c83156e4
Jeanet Lin <linett@gmail.com>
2022-04-30 14:29:27 +0200

    changed back to localhost

--- src/client.js
+++ src/client.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const client = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL,
+    /* baseURL: "http://localhost:4000", */
+    baseURL: process.env.REACT_APP_BACEKND,
+
 });
 
 client.interceptors.request.use((request) => {
