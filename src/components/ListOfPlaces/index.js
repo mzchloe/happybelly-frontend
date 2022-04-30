c2186c1afeed99358a4093c976ae40f6997869d0
Jeanet Lin <linett@gmail.com>
2022-04-30 14:15:04 +0200

    added sort newest first

--- src/components/ListOfPlaces/index.js
+++ src/components/ListOfPlaces/index.js
@@ -1,19 +1,30 @@
 import styles from "./ListOfPlaces.module.css";
 import { Place } from "../Place";
 
+export function ListOfPlaces({ places, handleDelete, favoritePlace=false }) {
 
-export function ListOfPlaces({ places, handleDelete }) {
   return (
     <ul className={styles.listContainer}>
-      {places.map((place) => {
-        return (
-          <div key={place._id}>
-            <Place place={place} handleDelete={handleDelete}
-            />
-            
-          </div>
-        );
-      }).sort((firstReview, secondReview) => firstReview.createdAt - secondReview.createdAt)}
+      {places
+        .sort((oldestReview, newestReview) => {
+          let oldestReviewDate = new Date(oldestReview.createdAt.toString())
+
+          let newestReviewDate = new Date(newestReview.createdAt.toString())
+       
+          if (oldestReviewDate.getTime() < newestReviewDate.getTime()) {
+            return 1
+          } if (oldestReviewDate.getTime() > newestReviewDate.getTime()) {
+            return -1 
+          } return 0
+  
+        })
+        .map((place) => {
+          return (
+            <div key={place._id}>
+              <Place place={place} handleDelete={handleDelete} favoritePlace={favoritePlace}/>
+            </div>
+          );
+        })}
     </ul>
   );
 }
