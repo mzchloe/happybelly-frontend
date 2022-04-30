232ef94001593cfab67ae25c86184ef3bef9783e
Jeanet Lin <linett@gmail.com>
2022-04-30 14:16:04 +0200

    displaying list of favorites

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,8 +1,20 @@
 import styles from "./Favorites.module.css";
+import { AuthContext } from "../../context";
+import { useContext } from "react";
+import { ListOfPlaces } from "../../components";
 
-export function Favorites() {
+export function Favorites(favorites) {
+    const { user } = useContext(AuthContext);
+    console.log(user)
+    if (!user) {
+        return 'Loading'
+    }
     return (
-        <div className={styles.container}>My Favorite Places</div>
+      <ul className={styles.listOfFavorites}>
+        <div className={styles.container}>My Favorite Places
+        <ListOfPlaces places={user.favorite}/>
+        </div>
+        </ul>
         
     )
 }
