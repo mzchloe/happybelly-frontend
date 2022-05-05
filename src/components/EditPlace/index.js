f47adad2a48ab553ec10222dcf986113030a3fed
Jeanet Lin <linett@gmail.com>
2022-05-05 21:35:04 +0200

    fixed issue of uncontrolled input

--- src/components/EditPlace/index.js
+++ src/components/EditPlace/index.js
@@ -1,30 +1,32 @@
 import styles from "./EditPlace.module.css";
-import { useState, useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { client } from "../../client";
-import { AuthContext } from "../../context";
-
 
 export function EditPlace() {
-
-  const [createdAt, setCreatedAt] = useState("");
-  //const [author, setAuthor] = useState("");
+  const [placeEdit, setPlaceEdit] = useState("");
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
-  const [city, setCity] = useState();
+  const [city, setCity] = useState("");
   const [dietaryType, setDietaryType] = useState("");
   const [description, setDescription] = useState("");
-const [placeEdit, setPlaceEdit] = useState("");
+  
 
   /* let navigate = useNavigate(); */
 
   //to get the data you want to edit from that place you chose to edit
   useEffect(() => {
     async function asyncCall() {
-        const editPlaceData = await client
-        .get(`place/${window.location.pathname.split('/').at(-1)}`);
-        setPlaceEdit(editPlaceData.data)
-        //console.log(editPlaceData)
+      const editPlaceData = await client.get(
+        `place/${window.location.pathname.split("/").at(-1)}`
+      );
+      setPlaceEdit(editPlaceData.data);
+     /*  setName(editPlaceData.name)
+      setAddress(editPlaceData.address)
+      setCity(editPlaceData.city)
+      setDietaryType(editPlaceData.dietaryType)
+      setDescription(editPlaceData.description)
+      //console.log(editPlaceData) */
     }
     asyncCall();
   }, []);
@@ -36,7 +38,7 @@ const [placeEdit, setPlaceEdit] = useState("");
     setCity(placeEdit.city);
     setDietaryType(placeEdit.dietaryType);
     setDescription(placeEdit.description);
-  }, [placeEdit]) 
+  }, [placeEdit]);
 
   let navigate = useNavigate();
 
@@ -44,89 +46,86 @@ const [placeEdit, setPlaceEdit] = useState("");
     e.preventDefault();
 
     const updatePlace = async () => {
-        try {
-            const data = await client.put(`place/${window.location.pathname.split('/').at(-1)}`, {
-                //createdAt,
-                //author,
-                name,
-                address,
-                city,
-                dietaryType,
-                description,
-              });
-              navigate('/places')  
-        } catch (error) {
-            
-        }
+      try {
+        const data = await client.put(
+          `place/${window.location.pathname.split("/").at(-1)}`,
+          {
+            name,
+            address,
+            city,
+            dietaryType,
+            description,
+          }
+        );
+        navigate("/places");
+      } catch (error) {}
     };
-    updatePlace()
-    
+    updatePlace();
   };
 
-
   return (
-    <div className={styles.editContainer}>
-    
+    <>
+    { placeEdit && <div className={styles.editContainer}>
       <form onSubmit={handleUpdate} className={styles.updatePlace}>
-       {/*  <h2>Edit Your Review</h2> */}
-        <label for="email" className={styles.required}>Name of place</label>
+        {/*  <h2>Edit Your Review</h2> */}
+        <label htmlFor="name" className={styles.required}>
+          Name of place
+        </label>
         <input
           id="name"
-          value={name}
+          value={name || ""}
           onChange={(e) => {
             setName(e.target.value);
           }}
           placeholder="Name of the restaurant*"
         />
-         <label for="address">Address</label>
+        <label htmlFor="address">Address</label>
         <input
           id="address"
-          value={address}
+          value={address || ""}
           onChange={(e) => {
             setAddress(e.target.value);
           }}
           placeholder="Address of the restaurant"
         />
-         <label for="city" className={styles.required}>City location</label>
+        <label htmlFor="city" className={styles.required}>
+          City location
+        </label>
         <input
           id="city"
-          value={city}
+          value={city || ""}
           onChange={(e) => {
             setCity(e.target.value);
           }}
           placeholder="Which city is it located at?*"
         />
         {/* Change to a dropdown menu */}
-        <label for="dietaryType" className={styles.required}>Special Dietary Needs</label>
+        <label htmlFor="dietaryType" className={styles.required}>
+          Special Dietary Needs
+        </label>
         <input
           id="dietaryType"
-          value={dietaryType}
+          value={dietaryType || ""}
           onChange={(e) => {
             setDietaryType(e.target.value);
           }}
           placeholder="Is it gluten-free/coeliac, lactose- or diary-free, FODMAP-friendly?*"
         />
-        <label for="description" className={styles.required}>Share your experience</label>
+        <label htmlFor="description" className={styles.required}>
+          Share your experience
+        </label>
         <textarea
           id="description"
-          value={description}
+          value={description || ""}
           onChange={(e) => {
             setDescription(e.target.value);
-          }} 
+          }}
           placeholder="Please share the experience you had with other members, it can be a dish you had, or special request you made.*"
         />
         <span className={styles.smallText}>(*) required fields</span>
         <button className={styles.saveBtn}>SAVE CHANGES</button>
       </form>
-    </div>
+    </div>}
+    </>
   );
 }
-/* 
-const name = "berlinEatery"
-const city = "berlin"
-
-const addPlace = () => {
-    name,
-    city
-}
- */
