49897b2be681b24832b2ca2b8b50803f03371e9f
Jeanet Lin <linett@gmail.com>
2022-05-06 18:50:04 +0200

    added a function to delete the whole favorite list

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -2,19 +2,31 @@ import styles from "./Favorites.module.css";
 import { AuthContext } from "../../context";
 import { useContext } from "react";
 import { ListOfPlaces } from "../../components";
+import { client } from "../../client";
 
 export function Favorites(favorites) {
-    const { user } = useContext(AuthContext);
-    //console.log(user)
-    if (!user) {
-        return 'Loading'
-    }
-    return (
-      <ul className={styles.listOfFavorites}>
-       {user && <div className={styles.container}>My Favorite Places
-        <ListOfPlaces places={user.favorite}/>
-        </div>}
-        </ul>
-        
-    )
+  const { user, setUser } = useContext(AuthContext);
+  //console.log(user)
+  const handleDeleteList = async () => {
+    const response = await client.delete(`/user/favorites`);
+
+    setUser(response.data);
+  };
+
+  if (!user) {
+    return "Loading";
+  }
+  return (
+    <ul className={styles.listOfFavorites}>
+      {user && (
+        <div className={styles.container}>
+          My Favorite Places
+          {user.favorite?.length > 0 && (
+            <button onClick={handleDeleteList}>Remove List</button>
+          )}
+          <ListOfPlaces places={user.favorite} />
+        </div>
+      )}
+    </ul>
+  );
 }
